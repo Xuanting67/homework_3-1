366aec9d9bcd5343678415390d9f91a7dbfaa469
xd67 <Xiaohetao1>
2022-03-15 19:41:28 -0400

    hw3-1

---
+++ serve_hw3.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vcm\Desktop\hw3
+git pull https://%TESTAPP_GIT_PAT%@github.com/Xuanting67/homework_3-1.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
